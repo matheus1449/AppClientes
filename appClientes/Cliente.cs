1f55ae9d441060fe14a5b02d0f5810ce59e23fbd
Matheus Francisco <matheus.1449@outlook.com>
2024-02-15 17:18:02 -0300

    sistema de cadastro de clientes

---
+++ appClientes/Cliente.cs
@@ -0,0 +1,14 @@
+namespace Cadastro;
+
+public class Cliente
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; }
+
+    public DateOnly DataNascimento { get; set; }
+
+    public DateTime CadastradoEm { get; set; }
+
+    public decimal Desconto { get; set; }
+}
